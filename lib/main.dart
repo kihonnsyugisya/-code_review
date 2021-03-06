dffd845824f68dbe6ea1007f024bc0314a975c9f
田辺 <katuobrothers@icloud.com>
2021-03-06 09:30:46 +0900

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,53 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+      MyApp()
   );
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return     MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 100,
+                color: Colors.red,
+              ),
+              Row(
+
+                children: [
+                  Container(
+                    color: Colors.yellow,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.green,
+                    width: 100,
+                    height: 100,
+                  ),
+                ],
+              ),
+              // SizedBox(
+              //   width: 209,
+              // ),
+              Container(
+                width: 100,
+                // height: 100,
+                color: Colors.blue,
+                // child: Text('Container2'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
